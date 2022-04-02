1972416043a8d9067fc0ee0db52eb3e949e1a91e
Igor Rivero <paulo.igor.rivero@gmail.com>
2022-04-02 14:55:40 -0300

    working with metrics

---
+++ Reviews_analysis.py
@@ -0,0 +1,135 @@
+#pip install google_trans_new 
+#pip install --upgrade pandas
+#Imports
+from email import header
+from operator import index
+from google_trans_new import google_translator        
+#from google.colab import drive 
+from deep_translator import GoogleTranslator
+from textblob import TextBlob
+import pandas as pd
+import csv
+import nltk
+nltk.download('stopwords')
+from nltk.tokenize import TweetTokenizer
+nltk.download('punkt')
+import numpy as np
+import time
+from time import sleep
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.preprocessing import MaxAbsScaler
+#import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+#%matplotlib inline
+#from wordcloud import WordCloud, STOPWORDS #to plot word cloud 
+
+
+#Reading a file with the reviews translated
+file_path ='C:/Users/paulo/Documents/projects/trip-ad-scrap/Output_scrap_reviews/Restaurants_Review_Coco_Bambu_rank8_EN.csv'
+path_splited = file_path.split('/')
+path_to_file_en ="C:/Users/paulo/Documents/projects/trip-ad-scrap/Output_scrap_reviews/Translated_Datasets/" + path_splited[-1]
+
+df_reviews_en = pd.read_csv(path_to_file_en)
+
+#df_reviews = pd.read_csv(path_to_file_en)
+
+####################################################################################################################################
+
+
+
+# Create a function to get the subjectivity
+def getSubjectivity(text):
+   frase = TextBlob(text)
+   return frase.sentiment.subjectivity
+ 
+
+# Create a function to get the polarity
+def getPolarity(text):
+   frase = TextBlob(text)
+   return frase.sentiment.polarity
+
+# Function to label the analysis 
+def getAnalysis(score):
+  if score < 0:
+    return 'Negative'
+  elif score == 0:
+    return 'Neutral'
+  else:
+    return 'Positive'
+
+# Function to label the SCORE 
+def getScore_analysis(score):
+  if score < 30:
+    return 'Negative'
+  elif score == 30:
+    return 'Neutral'
+  else:
+    return 'Positive'
+# Create two new columns 'Subjectivity' & 'Polarity'
+
+
+df_reviews_en['Subjectivity'] = df_reviews_en['REVIEW_en'].apply(getSubjectivity)
+df_reviews_en['Polarity'] = df_reviews_en['REVIEW_en'].apply(getPolarity)
+df_reviews_en['Analysis'] = df_reviews_en['Polarity'].apply(getAnalysis)
+
+df_reviews_en['SCORE_Analysis'] = df_reviews_en['SCORE'].apply(getScore_analysis)
+
+# Show the new dataframe with columns 'Subjectivity' & 'Polarity'
+###print(df_reviews_en[df_reviews_en.Polarity == 0])
+
+
+#Normalizing SCORE and POLARITY to match the same proportions
+#df = df_reviews_en['SCORE']
+#scaler = MaxAbsScaler()
+#scaler.fit(df)
+#scaled = scaler.transform(df)
+#scaled_df = pd.DataFrame(scaled, columns=['SCORE'])
+
+#print(scaled)
+#df_reviews_en = df_reviews_en[df_reviews_en.Analysis != df_reviews_en.SCORE_Analysis] #PAY ATTENTIOOOOON
+
+df = df_reviews_en
+print(df) 
+#def absolute_maximum_scale(series):
+#    return series / series.abs().max()
+'''columns_to_scale = 'SCORE'
+for col in columns_to_scale:
+    df[col] = absolute_maximum_scale(df[col])
+    print(col)'''
+#df['SCORE_N'] = absolute_maximum_scale(df['SCORE'])
+
+def min_max_scaling(series):
+    return (series - series.min()) / (series.max() - series.min())
+
+columns_to_scale = ('SCORE','Polarity')
+for col in columns_to_scale:
+    df[col] = min_max_scaling(df[col])
+df['Polarity'] = df['Polarity'].fillna(0.5)
+print(df)    
+print(df['SCORE'].max())
+#print(df['SCORE_N'].min())      
+print(df['Polarity'].max())
+
+powerbi_path ="C:/Users/paulo/projects/trip-ad-scrap/Output_scrap_reviews/to_test_on_pbi/Coco_Bambu_rank8_TRANSFORMED.csv"
+df.to_csv(powerbi_path)
+
+
+csvFile = open(powerbi_path, 'a', encoding="utf-8")
+csvWriter = csv.writer(csvFile)
+#csvWriter.writerows(df)
+
+
+
+value1 = df['SCORE'] 
+value2 = df['Polarity']
+#value3 = [8, 9, 10, 15, 20]
+
+results1 = pd.DataFrame({'SCORE': value1, 'Polarity': value2})
+
+results1.plot()
+plt.legend(loc='lower right')
+plt.xlabel("index")
+plt.ylabel("test2")
+plt.show()
---
+++ mozi.py
@@ -0,0 +1,23 @@
+def baz():
+    n = 100
+    while n > 0:
+        print(n)
+    return
+
+#baz()
+
+
+def spam():
+    while "0":
+        print('yes')
+    return
+
+#spam()    
+
+
+def foo():
+    while True:
+        print('t')
+    return
+
+foo()
